8c54d087d5e8208533f3d1c6a97c95a62d69f8b4
Bjarki Ágúst Guðmundsson <bjarki.agust@gmail.com>
2017-10-11 16:15:23 +0000

    Switch default sage invocation

--- calculate.sh
+++ calculate.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 K=$1
 
-# SAGE='sage'
-SAGE='docker run -t -i sagemath/sagemath sage'
+SAGE='sage'
+# SAGE='docker run -t -i sagemath/sagemath sage'
 
 mkdir -p output
 python gen_forbidden_subfigures.py $K | python dfa_for_all_patterns.py $K 0 1 > output/dfa_${K}.txt
